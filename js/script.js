442a80db3d6c97a320e85094dcf06a14dc0cb885
Pranto Deb <rajprit0m@outlook.com>
2024-02-13 00:25:03 +0600

    Parallelogram calculate completed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const triangleCalcBtn = document.getElementById("triangleCalcBtn");
 const rectangleCalcBtn = document.getElementById("rectangleCalcBtn");
-
+const parallelogramBtn = document.getElementById("parallelogramBtn");
+// triangle
 triangleCalcBtn.addEventListener("click", () => {
   const base = getInputValueById("userBaseNum");
   const hight = getInputValueById("userHightNum");
@@ -16,7 +17,7 @@ triangleCalcBtn.addEventListener("click", () => {
   const area = 0.5 * base * hight;
   document.getElementById("triangleDisplay").innerText = area;
 });
-
+// rectangle
 rectangleCalcBtn.addEventListener("click", () => {
   const width = getInputValueById("userWidthNum");
   const length = getInputValueById("userLengthNum");
@@ -31,4 +32,20 @@ rectangleCalcBtn.addEventListener("click", () => {
   }
   const area = width * length;
   document.getElementById("rectangleDisplay").innerText = area;
+});
+// parallelogram
+parallelogramBtn.addEventListener('click', ()=>{
+  const base = getInputValueById("userBaseNum2");
+  const hight = getInputValueById("userHeightNum2");
+  const positiveVali = positiveNumberValidation(base, hight);
+  const iNaNVali = iNaNValidation(base, hight);
+  if (positiveVali !== true) {
+    alert("base and hight must be positive value!");
+    return 0;
+  } else if (iNaNVali !== true) {
+    alert("input field required!");
+    return 0;
+  }
+  const area =  base * hight;
+  document.getElementById("parallelogramDisplay").innerText = area;
 });

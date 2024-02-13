312ea5ff43ae2cc07c4f6da504ae26e0819ca2c5
Pranto Deb <rajprit0m@outlook.com>
2024-02-13 14:20:26 +0600

    rhombusBtn completed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const triangleCalcBtn = document.getElementById("triangleCalcBtn");
 const rectangleCalcBtn = document.getElementById("rectangleCalcBtn");
 const parallelogramBtn = document.getElementById("parallelogramBtn");
+const rhombusBtn = document.getElementById("rhombusBtn");
 // triangle
 triangleCalcBtn.addEventListener("click", () => {
   const base = getInputValueById("userBaseNum");
@@ -34,7 +35,7 @@ rectangleCalcBtn.addEventListener("click", () => {
   document.getElementById("rectangleDisplay").innerText = area;
 });
 // parallelogram
-parallelogramBtn.addEventListener('click', ()=>{
+parallelogramBtn.addEventListener("click", () => {
   const base = getInputValueById("userBaseNum2");
   const hight = getInputValueById("userHeightNum2");
   const positiveVali = positiveNumberValidation(base, hight);
@@ -46,6 +47,22 @@ parallelogramBtn.addEventListener('click', ()=>{
     alert("input field required!");
     return 0;
   }
-  const area =  base * hight;
+  const area = base * hight;
   document.getElementById("parallelogramDisplay").innerText = area;
+});
+// rhombusBtn
+rhombusBtn.addEventListener("click", () => {
+  const d1 = getInputValueById("userD1");
+  const d2 = getInputValueById("userD2");
+  const positiveVali = positiveNumberValidation(d1, d2);
+  const iNaNVali = iNaNValidation(d1, d1);
+  if (positiveVali !== true) {
+    alert("d1 and d2 must be positive value!");
+    return 0;
+  } else if (iNaNVali !== true) {
+    alert("input field required!");
+    return 0;
+  }
+  const area = 0.5 * d1 * d2;
+  document.getElementById("rhombusDisplay").innerText = area;
 });
